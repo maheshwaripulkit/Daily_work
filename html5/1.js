63037520e62764c48289ff543e1776cbc0eb6f84
Pulkit Maheshwari <pulkitmaheshwari01@gmail.com>
2020-02-17 12:31:53 -0800

    html files

---
+++ html5/1.js
@@ -0,0 +1,22 @@
+<html>
+<body>
+<button onclick="myFunction()">Click..</button>
+
+<script>
+function myFunction()
+{
+	 var age=20;
+	 if(age>=18)
+	 {
+		  alert("You may vote");
+		  
+	 }
+	 else{
+		 alert("you can not vote ");
+		 
+		 }
+}
+
+</script>
+</body>
+<html>		 
